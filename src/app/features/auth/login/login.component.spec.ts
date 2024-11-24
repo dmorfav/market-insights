ccf3bc166238059f8a61080d62925c6dce17a923
Daniel Morfa Vega <dmorfav@gmail.com>
2024-11-24 22:22:54 +0100

    feat(testing): add testing for mock service

--- src/app/features/auth/login/login.component.spec.ts
+++ src/app/features/auth/login/login.component.spec.ts
@@ -1,17 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+import {signal} from "@angular/core";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let mockAuthService: jasmine.SpyObj<AuthService>;
+  let mockRouter: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
+    mockAuthService = jasmine.createSpyObj('AuthService', ['login']);
+    mockRouter = jasmine.createSpyObj('Router', ['navigate']);
+
     await TestBed.configureTestingModule({
-      imports: [LoginComponent, NoopAnimationsModule]
+      imports: [LoginComponent, ReactiveFormsModule, NoopAnimationsModule],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: Router, useValue: mockRouter }
+      ]
     })
-    .compileComponents();
+        .compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
@@ -20,5 +32,27 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call authService.login and navigate on successful login', () => {
+    const loginResponse = signal(true);
+    mockAuthService.login.and.returnValue(loginResponse);
+
+    component['loginForm'].setValue({ email: 'admin@example.com', password: 'admin123' });
+    component.onSubmit();
+
+    expect(mockAuthService.login).toHaveBeenCalledWith('admin@example.com', 'admin123');
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/dashboard']);
+  });
+
+  it('should show alert on failed login', () => {
+    const loginResponse = signal(false);
+    mockAuthService.login.and.returnValue(loginResponse);
+
+    spyOn(window, 'alert');
+    component['loginForm'].setValue({ email: 'invalid@example.com', password: 'wrongpassword' });
+    component.onSubmit();
+
+    expect(window.alert).toHaveBeenCalledWith('Invalid credentials');
   });
 });
