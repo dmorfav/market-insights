aec47289942dbdd0b736abbce39120090a5ddf6c
Daniel Morfa Vega <daniel.morfa@uvesolutions.com>
2024-11-24 14:05:27 +0100

    feat(dashboard): add basic auth, structure of main dashboard, material design integrations

--- src/app/features/dashboard/pages/main-dashboard/main-dashboard.component.ts
+++ src/app/features/dashboard/pages/main-dashboard/main-dashboard.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {MatGridList, MatGridTile} from '@angular/material/grid-list';
+import {MenuHeaderComponent} from '../../../../shared/components/menu-header/menu-header.component';
+import {RouterOutlet} from '@angular/router';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
 
 @Component({
   selector: 'app-main-dashboard',
-  imports: [],
+  imports: [MatGridList, MatGridTile, MenuHeaderComponent, RouterOutlet, MatCard, MatCardContent, MatCardHeader, MatCardTitle],
   templateUrl: './main-dashboard.component.html',
   standalone: true,
   styleUrl: './main-dashboard.component.scss'
