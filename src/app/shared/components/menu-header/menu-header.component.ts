552ae8757541add6456f722db4779a3afe87fd31
Daniel Morfa Vega <daniel.morfa@uvesolutions.com>
2024-11-24 14:33:45 +0100

    fix(QA): remove unused imports
    based in new angular feature warning alert in build step

--- src/app/shared/components/menu-header/menu-header.component.ts
+++ src/app/shared/components/menu-header/menu-header.component.ts
@@ -1,13 +1,13 @@
 import {Component, inject} from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
-import {MatButton, MatIconButton} from '@angular/material/button';
+import {MatIconButton} from '@angular/material/button';
 import {AuthService} from '../../../core/services/auth.service';
 import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-menu-header',
-  imports: [MatToolbar, MatIcon, MatIconButton, MatButton],
+  imports: [MatToolbar, MatIcon, MatIconButton],
   templateUrl: './menu-header.component.html',
   styleUrl: './menu-header.component.scss',
   standalone: true
