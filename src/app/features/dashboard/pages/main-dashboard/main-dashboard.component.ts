38373377d71a8df430613eea1579a2376ef5cc24
Daniel Morfa Vega <daniel.morfa@uvesolutions.com>
2024-11-24 02:17:48 +0100

    feat(dashboard): add main dashboard

---
+++ src/app/features/dashboard/pages/main-dashboard/main-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-dashboard',
+  imports: [],
+  templateUrl: './main-dashboard.component.html',
+  standalone: true,
+  styleUrl: './main-dashboard.component.scss'
+})
+export class MainDashboardComponent {
+
+}
