77c385d9d39e0d03bab0b852b3a047d94770b579
Daniel Morfa Vega <dmorfav@gmail.com>
2024-11-24 21:15:52 +0100

    refactor(QA): remove unused imports

--- src/app/features/dashboard/pages/main-dashboard/main-dashboard.component.ts
+++ src/app/features/dashboard/pages/main-dashboard/main-dashboard.component.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatGridList, MatGridTile} from '@angular/material/grid-list';
-import {MenuHeaderComponent} from '../../../../shared/components/menu-header/menu-header.component';
-import {RouterOutlet} from '@angular/router';
 import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
 
 @Component({
   selector: 'app-main-dashboard',
-  imports: [MatGridList, MatGridTile, MenuHeaderComponent, RouterOutlet, MatCard, MatCardContent, MatCardHeader, MatCardTitle],
+  imports: [MatGridList, MatGridTile, MatCard, MatCardContent, MatCardHeader, MatCardTitle],
   templateUrl: './main-dashboard.component.html',
   standalone: true,
   styleUrl: './main-dashboard.component.scss'
