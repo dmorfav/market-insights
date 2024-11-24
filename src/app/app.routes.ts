52364607027670c62932d46d4ad826d1c3ac93e5
Daniel Morfa Vega <dmorfav@gmail.com>
2024-11-24 02:17:48 +0100

    feat(dashboard): add main dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {MainDashboardComponent} from './features/dashboard/pages/main-dashboard/main-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Main Dashboard',
+    component: MainDashboardComponent
+  }
+];
