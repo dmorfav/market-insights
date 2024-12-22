9ccbd11c716e8fdae2b56114142e9edc642b5b90
Daniel Morfa Vega <daniel.morfa@uvesolutions.com>
2024-12-22 23:05:31 +0100

    prevent right click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,10 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'marketInsights';
+  ngOnInit() {
+    // Disable right click
+    document.addEventListener('contextmenu', (e) => e.preventDefault());
+  }
 }
