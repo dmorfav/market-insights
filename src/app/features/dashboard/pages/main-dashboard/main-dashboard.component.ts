70704886e52e3399680f2774dc7060469f995a43
Daniel Morfa Vega <daniel.morfa@uvesolutions.com>
2024-12-25 16:03:52 +0100

    refactor(symbol): abstract symbol component
    Extract component for handle a granular structure and reuse later

--- src/app/features/dashboard/pages/main-dashboard/main-dashboard.component.ts
+++ src/app/features/dashboard/pages/main-dashboard/main-dashboard.component.ts
@@ -1,21 +1,22 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {MatGridList, MatGridTile} from '@angular/material/grid-list';
 import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
-import {FinanceService} from '../../../../core/services/finance/finance.service';
+import {SymbolListComponent} from '../../../../shared/components/symbol-list/symbol-list.component';
 
 @Component({
   selector: 'app-main-dashboard',
-  imports: [MatGridList, MatGridTile, MatCard, MatCardContent, MatCardHeader, MatCardTitle],
   templateUrl: './main-dashboard.component.html',
+  styleUrls: ['./main-dashboard.component.scss'],
   standalone: true,
-  styleUrl: './main-dashboard.component.scss'
+  imports: [
+    MatGridList,
+    MatGridTile,
+    MatCard,
+    MatCardTitle,
+    MatCardHeader,
+    MatCardContent,
+    SymbolListComponent
+  ]
 })
-export class MainDashboardComponent implements OnInit {
-
-  private readonly financeService = inject(FinanceService);
-
-  ngOnInit(): void {
-    const data = this.financeService.getHistoricalData('AAPL', '2021-01-01', '2021-12-31');
-    console.log(data());
-  }
+export class MainDashboardComponent {
 }
