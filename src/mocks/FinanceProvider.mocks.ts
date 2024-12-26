51420c2e73ca124f198a25aa9d15551b76a61d38
Daniel Morfa Vega <dmorfav@gmail.com>
2024-12-26 01:17:36 +0100

    test(mocks): add mocks methods

--- src/mocks/FinanceProvider.mocks.ts
+++ src/mocks/FinanceProvider.mocks.ts
@@ -1,12 +1,32 @@
 // Mock del proveedor
 import {FinanceProviderInterface} from '../app/shared/models/Interface/finance-provider-interface';
-import {signal} from '@angular/core';
+import {signal, WritableSignal} from '@angular/core';
+import {ExSymbol} from '../app/shared/models/Interface/symbol';
+import {HistoricalData} from '../app/shared/models/Interface/historical-data';
 
 export class MockFinanceProvider implements FinanceProviderInterface {
-  getHistoricalData(symbol: string, startDate: string, endDate: string) {
-    return signal([{ date: '2024-01-01', open: 100, high: 105, low: 95, close: 100, volume: 1000 }]);
+
+  getRealTimeData(_symbol: string) {
+    return signal({ symbol: _symbol, price: 150, change: 2, volume: 5000, timestamp: '2024-01-01' });
   }
-  getRealTimeData(symbol: string) {
-    return signal({ symbol: 'AAPL', price: 150, change: 2, volume: 5000, timestamp: '2024-01-01' });
+
+  getSymbolList(): WritableSignal<ExSymbol[]> {
+    return signal([
+      { description: '', symbol: 'AAPL', displaySymbol: 'Apple Inc.' },
+      { description: '', symbol: 'GOOGL', displaySymbol: 'Alphabet Inc.' }
+    ]);
+  }
+
+  getDataBySymbol(symbol: string): WritableSignal<HistoricalData> {
+    return signal({
+      date: 3600,
+      open: 150,
+      close: 152,
+      change: 2,
+      high: 155,
+      low: 148,
+      percentChange: 1.33,
+      previousClose: 150
+    });
   }
 }
