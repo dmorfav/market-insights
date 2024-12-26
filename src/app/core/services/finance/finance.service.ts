f2fb5e385f247e0debe5bc6eb6b5a65e90ef0de7
Daniel Morfa Vega <dmorfav@gmail.com>
2024-12-26 01:25:28 +0100

    test(service): add testing for new method

--- src/app/core/services/finance/finance.service.ts
+++ src/app/core/services/finance/finance.service.ts
@@ -1,6 +1,9 @@
-import {inject, Injectable} from '@angular/core';
+import {inject, Injectable, WritableSignal} from '@angular/core';
 import {FINANCE_PROVIDER} from '../../providers/finance.provider';
 import {FinanceProviderInterface} from '../../../shared/models/Interface/finance-provider-interface';
+import {HistoricalData} from '../../../shared/models/Interface/historical-data';
+import {RealTimeData} from '../../../shared/models/Interface/real-time-data';
+import {ExSymbol} from '../../../shared/models/Interface/symbol';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +12,15 @@ export class FinanceService {
 
   private readonly financeService = inject<FinanceProviderInterface>(FINANCE_PROVIDER);
 
-  getHistoricalData(symbol: string, startDate: string, endDate: string) {
-    return this.financeService.getHistoricalData(symbol, startDate, endDate);
+  getDataBySymbol(symbol: string): WritableSignal<HistoricalData> {
+    return this.financeService.getDataBySymbol(symbol);
   }
 
-  getRealTimeData(symbol: string) {
+  getRealTimeData(symbol: string): WritableSignal<RealTimeData> {
     return this.financeService.getRealTimeData(symbol);
+  }
+
+  getSymbolList(): WritableSignal<ExSymbol[]> {
+    return this.financeService.getSymbolList();
   }
 }
