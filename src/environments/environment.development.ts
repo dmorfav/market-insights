2c84b5f3fc874839ed94642647bc315b516489dd
Daniel Morfa Vega <dmorfav@gmail.com>
2025-07-16 23:43:13 +0200

    feat(symbol-list): agregar funcionalidad para mostrar solo favoritos y optimizar la lista de símbolos

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   FINNHUB_API_KEY: import.meta.env.NG_APP_FINNHUB_API_KEY,
   FINNHUB_API_URL: import.meta.env.NG_APP_FINNHUB_API_URL
 };
